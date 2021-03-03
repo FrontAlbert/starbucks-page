09950ecd2583df3e1413057a2696ff43e8ba0f81
FrontAlbert <alberthui@outlook.com>
2021-03-03 00:19:59 -0500

    push to github

---
+++ main.js
@@ -0,0 +1,8 @@
+function imgSlider(x){
+    document.querySelector(".starbucks").src = x;
+}
+
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
